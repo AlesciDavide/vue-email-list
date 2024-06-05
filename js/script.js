2279b4fa108ac9d155bee333b0fb68ce605739d6
AlesciDavide <davi.alesci@email.com>
2024-06-05 13:32:32 +0200

    email generate tramite api, inserite in un'array e stampate in pagina

--- js/script.js
+++ js/script.js
@@ -3,11 +3,22 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-
+            arrayListEmail: [],
         }
     },
     methods:{
-        
+        getData(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    this.arrayListEmail.push(response.data.response);
+                });
+            };
+            console.log(this.arrayListEmail);
+        },
+    },
+
+    created(){
+        this.getData();
     }
 
 }).mount('#app')
