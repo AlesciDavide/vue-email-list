7306ebeaf04936d8969745a7f67c5378391b5b2d
AlesciDavide <davi.alesci@email.com>
2024-06-05 15:13:59 +0200

    v.2 senza v-if senza limite di email

--- js/script.js
+++ js/script.js
@@ -4,16 +4,22 @@ createApp({
     data(){
         return{
             arrayListEmail: [],
+            arrayListEmailMoment: [],
         }
     },
     methods:{
         getData(){
+
             for(let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                    this.arrayListEmail.push(response.data.response);
+                    /* this.arrayListEmail.push(response.data.response); */
+                    this.arrayListEmailMoment.push(response.data.response);
                 });
             };
-            console.log(this.arrayListEmail);
+            this.copyArray();
+        },
+        copyArray: function(){
+            this.arrayListEmail = this.arrayListEmailMoment
         },
     },
     created(){
